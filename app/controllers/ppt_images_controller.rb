5632bb8f6b23b4a79495c7166343a98713ba710f
hengtangan2025 <504371515@qq.com>
2015-10-29 17:46:59 +0800

    模型修改

--- app/controllers/ppt_images_controller.rb
+++ app/controllers/ppt_images_controller.rb
@@ -9,8 +9,19 @@ class PptImagesController < ApplicationController
   end
 
   def create
+    @deck = Powerpoint::Presentation.new
     @ppt_image = @ppt.ppt_images.new(ppt_image_params)
+
     if @ppt_image.save
+      @ppt.ppt_images.each do |ppt_image|
+        ppt_image.images.each do |image|
+          coords = {x: 0, y: 0, cx: 720 * 12700, cy: 540 * 12700}
+          @deck.add_pictorial_slide image.title, Rails.root + "app/assets/images/" + image.title, coords
+        end
+      end
+
+      @deck.save(Rails.root + "app/assets/pptxs/#{@ppt.title}.pptx")
+      
       redirect_to "/ppts/#{@ppt.id}"
     end
   end
--- app/controllers/ppts_controller.rb
+++ app/controllers/ppts_controller.rb
@@ -8,23 +8,7 @@ class PptsController < ApplicationController
   end
 
   def create
-    # @deck = Powerpoint::Presentation.new
-    # @ppt_images = PptImage.all
-    # @word_contents = WordContent.all
     ppt = Ppt.create(ppt_params)
-    # coords = {x: 0, y: 0, cx: 720 * 12700, cy: 540 * 12700}
-
-    # @word_contents.each do |word_content|
-    #   @deck.add_intro word_content.title, word_content.content
-    # end
-
-    # @ppt_images.each do |ppt_image|
-    #   ppt_image.image.each do |image|
-    #     @deck.add_pictorial_slide image.title, Rails.root + "app/assets/images/" + image.title,coords
-    #   end
-    # end
-    
-    # @deck.save(file.name + '.pptx')
 
     if ppt.save     
       redirect_to "/ppts"
@@ -37,7 +21,6 @@ class PptsController < ApplicationController
 
   def download
     ppt = Ppt.find(params[:id])
-    # send_file("#{Rails.root}/" + ppt.title + ".pptx", :filename => ppt.title + ".pptx")
     send_file("#{Rails.root}/app/assets/pptxs/#{ppt.title}.pptx", :filename => ppt.title + ".pptx")
   end
 
